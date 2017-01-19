d73688044e61aaa3bc34e52cc08398a08cedbbe6
Anyandsi <andrey5577990@gmail.com>
2017-01-20 00:03:36 +0200

    Настольная лампа. Обычный искусственный источник

---
+++ TablePanel.java
@@ -0,0 +1,6 @@
+public class TablePanel extends Lamp {
+    public TablePanel(String sourceName, int brightnessLevel, boolean isTurnedOn, int counter) {
+        super(sourceName, 100, isTurnedOn, counter);
+    }
+
+}
